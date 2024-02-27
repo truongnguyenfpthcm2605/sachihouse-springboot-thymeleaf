262a4aa025e90a25d95b19fcdb5d7d64a0c224eb
nguyenvantruong <truongnguenlqm@gmail.com>
2024-02-27 16:52:56 +0700

    update homeController

--- src/main/java/com/shachi/shachihouse/controller/HomeController.java
+++ src/main/java/com/shachi/shachihouse/controller/HomeController.java
@@ -2,27 +2,21 @@ package com.shachi.shachihouse.controller;
 
 import com.shachi.shachihouse.entities.Category;
 import com.shachi.shachihouse.entities.House;
-import com.shachi.shachihouse.service.CategoryService;
 import com.shachi.shachihouse.service.impl.CategoryServiceImpl;
-import com.shachi.shachihouse.service.impl.FileServiceImpl;
 import com.shachi.shachihouse.service.impl.HouseServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 
-import java.io.IOException;
+
 import java.util.*;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
-    private final FileServiceImpl fileService;
+
     private final HouseServiceImpl houseService;
     private final CategoryServiceImpl categoryService;
 
@@ -158,19 +152,7 @@ public class HomeController {
     }
 
 
-    @PostMapping ("/uploadFile")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
-    public String upload(@RequestParam("file")MultipartFile multipartFile,@RequestParam("folder") String folderName) throws IOException {
-        return fileService.uploadFileString(multipartFile,folderName);
 
-    }
-    @PostMapping ("/downloadFile")
-    @ResponseBody
-    @ResponseStatus(HttpStatus.OK)
-    public String upload(@RequestParam("fileName") String fileName,@RequestParam("folder") String folderName)  {
-        return fileService.read(folderName,fileName);
-    }
 
 
 
